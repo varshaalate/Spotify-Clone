1d6b43d2799b72e0eec385a4a19ac4ab0bba3612
Varsha Alate <varshaalate86@gmail.com>
2025-07-02 12:30:34 +0530

    Updated the functinality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let songIndex=0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
 
 
 let songs=[
@@ -24,17 +25,20 @@ masterPlay.addEventListener('click',()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+                gif.style.opacity = 1;
+
         
     }
     else{
          masterPlay.classList.remove('fa-circle-pause');
         masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
     }
 })
 
 
 //LIsten To events
-document.addEventListener('timeupdate',()=>{
+myProgressBar.addEventListener('timeupdate',()=>{
     console.log("Time Update");
 
     // Update Seekbar
